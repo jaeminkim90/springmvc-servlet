59297da9b9f1193406e583bb8c67debc2c0a8a84
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-28 21:55:01 +0900

    FEAT: POST HTML Form 방식을 이용해서 서버로 데이터 전송

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 /**
  * 1. 파라미터 전송 기능
  * http://localhost:8080/request-param?username=hello&age=20
+ * <p>
+ * 2. 동일한 파라미터 전송 가능
+ * http://localhost:8080/request-param?username=hello&username=kim&age=20
  */
 @WebServlet(name = "RequestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
@@ -21,7 +24,6 @@ public class RequestParamServlet extends HttpServlet {
         request.getParameterNames().asIterator()
                 .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
-
 
         System.out.println();
         System.out.println("[단일 파라미터 조회] - start");
@@ -37,9 +39,7 @@ public class RequestParamServlet extends HttpServlet {
         for (String name : usernames) {
             System.out.println("name = " + name);
         }
+        response.getWriter().write("OK");
         System.out.println("[이름이 같은 복수 파라미터 조회] - end");
-
-
     }
-
 }
