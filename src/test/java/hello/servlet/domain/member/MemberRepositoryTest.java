5d850477dfd89557ae4153a4e619a0ac9377d482
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-29 16:03:33 +0900

    TEST: MemberRepository 단위 테스트 완료

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberRepositoryTest {
@@ -26,22 +29,24 @@ class MemberRepositoryTest {
 
         // then
         Member findMember = memberRepository.findById(savedMember.getId());
-        Assertions.assertThat(savedMember).isEqualTo(findMember);
-    }
-
-    @Test
-    void getInstance() {
-    }
-
-    @Test
-    void findById() {
+        assertThat(savedMember).isEqualTo(findMember);
     }
 
     @Test
     void findAll() {
-    }
 
-    @Test
-    void clearStore() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
     }
 }
