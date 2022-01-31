2587f3f12a7f7649163d25bf4a32a95e0ecbe387
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-31 14:31:55 +0900

    FEAT: 프런트 컨트롤러 V4 - 기존 구조에서 모델을 컨트롤러 파라미터로 넘기고, 뷰의 논리 이름을 반환하는 방식으로 프런트 컨트롤러 구현

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -28,6 +28,8 @@ public class MyView {
 
     // JSP가 request.getAttribute()로 데이터를 조회하기 때문에 모델에서 데이터를 꺼내 request.setAttribute 작업을 해야한다.
     private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+
+        // model은 단일 member 객체 또는 전체 Members가 담긴 List에 해당한다.
         model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,62 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+
+        // controllerMap에서 요청 URI에 해당하는 컨트롤러 객체를 반환
+        ControllerV4 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // paramMap = 모든 Parameter 데이터를 담은 Map
+        Map<String, String> paramMap = creatParamMap(request);
+        Map<String, Object> model = new HashMap<>(); // 추가
+
+        String viewName = controller.process(paramMap, model);
+
+        MyView view = viewResolver(viewName);
+
+        view.render(model, request, response);
+    }
+
+    // MyView 객체 생성 후 반환(논리 경로를 물리 경로로 변경하여 생성자 파라미터에 이용)
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> creatParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
