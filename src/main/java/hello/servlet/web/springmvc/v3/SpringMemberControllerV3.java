a38df0ffef4d876662abdbc0e6de6ee16c132dc6
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-01 12:01:53 +0900

    FEAT: @RequestMapping -> @GetMapping, @PostMapping으로 전환하여 코드를 더 간결하게 구현

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,25 +4,33 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
+/**
+ * v3
+ * Model 도입
+ * ViewName 직접 반환
+ *
+ * @RequestParam 사용
+ * @RequestMapping -> @GetMapping, @PostMapping
+ */
 @Controller
 @RequestMapping("/springmvc/v3/members")
 public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @GetMapping("/new-form")
     private String newForm() {
         return "new-form";
     }
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public String save(@RequestParam("username") String username,
-                             @RequestParam("age") int age,
-                             Model model) {
+                       @RequestParam("age") int age,
+                       Model model) {
 
         Member member = new Member(username, age);
         memberRepository.save(member);
@@ -32,7 +40,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+    @PostMapping()
     public String members(Model model) {
         List<Member> members = memberRepository.findAll();
 
